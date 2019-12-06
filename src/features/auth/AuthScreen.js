ca12d17e4cfe4ace8f9c6d2f83342100fb67419c
jeevaka-noetic <jeevaka.amarasekara@wearenoetic.com>
2019-12-07 00:37:18 +0530

    Firebase messaging permission added

--- src/features/auth/AuthScreen.js
+++ src/features/auth/AuthScreen.js
@@ -1,48 +1,77 @@
-import React, { PureComponent } from 'react'
-import { ActivityIndicator, View, StyleSheet} from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage'
-import SplashScreen from 'react-native-splash-screen'
-
+import React, {PureComponent} from 'react';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import SplashScreen from 'react-native-splash-screen';
+import firebase from 'react-native-firebase';
 class AuthScreen extends PureComponent {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+  async componentDidMount() {
+    try {
+      if (this.checkPermission()) {
+        this.checkedLoggedUser();
+      } else {
+        this.requestPermission();
+      }
+    } catch (error) {}
+    // firebase.messaging().subscribeToTopic('aaa');
+  }
+
+  async checkedLoggedUser() {
+    const value = await AsyncStorage.getItem('IsLoggedIn');
+    if (value !== null) {
+      this.props.navigation.navigate('Home');
+      SplashScreen.hide();
+    } else {
+      this.props.navigation.navigate('Landing');
+      SplashScreen.hide();
     }
-    async componentDidMount(){
-        try {
-            const value = await AsyncStorage.getItem('IsLoggedIn');
-            console.log(value)
-            
-            if (value !== null) {
-              this.props.navigation.navigate("Home")
-              SplashScreen.hide()
+  }
 
-            }
-            else{
-              this.props.navigation.navigate("Landing")
-              SplashScreen.hide()
-
-            }
-          } catch (error) {
-            // Error retrieving data
-          }
+  async checkPermission() {
+    firebase
+      .messaging()
+      .hasPermission()
+      .then(enabled => {
+        if (enabled) {
+          return true;
+        } else {
+          return false;
         }
-    render(){
-        return (
-          <View style={[styles.container, styles.horizontal]}>
-            <ActivityIndicator size="large" color="#3b4761" />
-          </View>
-          )
-    }
+      });
+  }
+
+  async requestPermission() {
+    firebase
+      .messaging()
+      .requestPermission()
+      .then(() => {
+        this.checkedLoggedUser();
+      })
+      .catch(error => {
+        // Todo : Need to add popup for request permission
+        console.log('permission rejected');
+      });
+  }
+
+  render() {
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" color="#3b4761" />
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center'
-    },
-    horizontal: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      padding: 10
-    }
-  })
-export default AuthScreen
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
+export default AuthScreen;
