727b61f9958c3a4584a7c19b77df3b8bf6bfe9a1
jeevaka-noetic <jeevaka.amarasekara@wearenoetic.com>
2019-12-31 19:49:34 +0530

    workshopReducer added

---
+++ src/reducers/workshopReducer.js
@@ -0,0 +1,30 @@
+import * as actions from "../actions/workshopActions"
+
+const initialState = {
+    isLoading: false,
+    initialLoading: true
+}
+
+export const workshopReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actions.SEND_FETCH_WORKSHOP_LIST_REQUEST:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case actions.SEND_FETCH_WORKSHOP_LIST_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+                initialLoading: false,
+                isLoading: false
+            }
+        case actions.SEND_FETCH_WORKSHOP_LIST_FAILURE:
+            return {
+                ...state            }
+        default:
+            return state
+    }
+}
+
+
