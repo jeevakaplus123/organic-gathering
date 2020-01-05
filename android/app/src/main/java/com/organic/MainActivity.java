9d8e845470801293041aeae5a96ef0835065adc9
jeevaka-noetic <jeevaka.amarasekara@wearenoetic.com>
2020-01-05 15:24:52 +0530

    Config updated

--- android/app/src/main/java/com/organic/MainActivity.java
+++ android/app/src/main/java/com/organic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.organic;
-
+import android.content.Intent; // <-- include if not already there
+import com.tkporter.sendsms.SendSMSPackage;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; // Import this.
 import android.os.Bundle;
@@ -11,6 +12,13 @@ public class MainActivity extends ReactActivity {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
+
+  @Override
+public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	super.onActivityResult(requestCode, resultCode, data);
+	//probably some other stuff here
+	SendSMSPackage.getInstance().onActivityResult(requestCode, resultCode, data);
+}
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
