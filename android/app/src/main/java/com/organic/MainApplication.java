6b2888e2e7e35ef3c0e9639f937b1626aed90666
Jeevaka <jeevaka.amarasekara@wearenoetic.com>
2020-07-26 17:04:09 +0530

    Add configuration for iOS

--- android/app/src/main/java/com/organic/MainApplication.java
+++ android/app/src/main/java/com/organic/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // Firebase Firestore
